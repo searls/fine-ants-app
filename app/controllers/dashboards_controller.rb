d24a6163064a9a9d8138117a05938f09384eab44
Justin Searls <searls@gmail.com>
2017-07-29 10:36:23 -0400

    add ytd change col (colored) and omit $0 accounts

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,5 +4,6 @@ class DashboardsController < ApplicationController
       account.snapshots.order("created_at").last(1)
     end
     @total = @snapshots.sum(&:amount)
+    @total_ytd_change = @snapshots.map(&:ytd_change).reduce(:+)
   end
 end
--- app/models/snapshot.rb
+++ app/models/snapshot.rb
@@ -2,4 +2,10 @@ class Snapshot < ActiveRecord::Base
   belongs_to :account
   monetize :amount_cents
 
+  def ytd_change
+    first_snapshot_this_year = account.snapshots.
+      where("strftime('%Y', created_at) = strftime('%Y', date('now'))").
+      order('created_at asc').first
+    amount - first_snapshot_this_year.amount
+  end
 end
