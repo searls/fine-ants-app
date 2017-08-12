802fe18c0b26122d91358cb7bbad7b6e4200946d
Justin Searls <searls@gmail.com>
2017-08-12 15:42:41 -0400

    Add weekly/monthly change rates, format currencies

--- lib/account_summaries.rb
+++ lib/account_summaries.rb
@@ -1,10 +1,14 @@
 class AccountSummaries
-  attr_reader :name, :last_updated, :ytd_change, :amount, :accounts, :empty_accounts
+  attr_reader :name, :last_updated, :week_change, :month_change, :ytd_change,
+              :amount,
+              :accounts, :empty_accounts
 
   def initialize(accounts)
     @accounts = accounts.map { |a| AccountSummary.new(a) }
     @name = "Totals"
     @last_updated = @accounts.map(&:last_updated).max
+    @week_change = @accounts.map(&:week_change).sum
+    @month_change = @accounts.map(&:month_change).sum
     @ytd_change = @accounts.map(&:ytd_change).sum
     @amount = @accounts.map(&:amount).sum
     @empty_accounts = @accounts.select { |a| a.amount.zero? }
@@ -12,12 +16,16 @@ class AccountSummaries
 end
 
 class AccountSummary
-  attr_reader :name, :last_updated, :ytd_change, :amount, :account
+  attr_reader :name, :last_updated, :week_change, :month_change, :ytd_change,
+              :amount,
+              :account
 
   def initialize(account)
     snapshot = account.most_recent_snapshot
     @name = account.full_name
     @last_updated = snapshot.created_at
+    @week_change = snapshot.change_since(1.week.ago.to_date)
+    @month_change = snapshot.change_since(1.month.ago.to_date)
     @ytd_change = snapshot.change_since(Date.today.beginning_of_year)
     @amount = snapshot.amount
     @account = account
