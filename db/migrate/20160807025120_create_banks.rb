d39fdcb70dc5fe0db16d20c6c52824f95ba2923b
Adam Lukens <adam@testdouble.com>
2017-08-12 17:14:57 -0400

    Version migrations for Rails 5.1

--- db/migrate/20160807025120_create_banks.rb
+++ db/migrate/20160807025120_create_banks.rb
@@ -1,4 +1,4 @@
-class CreateBanks < ActiveRecord::Migration
+class CreateBanks < ActiveRecord::Migration[5.1]
   def change
     create_table :banks do |t|
       t.string :name
--- db/migrate/20160807140737_uniq_accounts.rb
+++ db/migrate/20160807140737_uniq_accounts.rb
@@ -1,4 +1,4 @@
-class UniqAccounts < ActiveRecord::Migration
+class UniqAccounts < ActiveRecord::Migration[5.1]
   def change
     add_index :accounts, [:bank_id, :foreign_id], unique: true
   end
