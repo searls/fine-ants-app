e1196074d4ce9e44470c4841ffd1d42dfbd51852
Justin Searls <searls@gmail.com>
2017-10-07 13:12:39 -0400

    update things

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.web_console.whitelisted_ips = '70.194.13.32'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
