c68a88ded56514adf05a5e6057819fdde58b50ba
Justin Searls <searls@gmail.com>
2019-04-27 11:13:12 -0400

    Upgrade webpacker i hope

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,13 +1,29 @@
 const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
 
-environment.plugins.set(
+environment.plugins.prepend(
   'Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery',
-    jquery: 'jquery'
+    jquery: 'jquery',
+    'window.jQuery': 'jquery'
   })
 )
+
+environment.config.merge({
+  module: {
+    rules: [{
+      test: require.resolve('jquery'),
+      use: [{
+        loader: 'expose-loader',
+        options: 'jQuery'
+      },{
+        loader: 'expose-loader',
+        options: '$'
+      }]
+    }]
+  }
+})
 
 module.exports = environment
